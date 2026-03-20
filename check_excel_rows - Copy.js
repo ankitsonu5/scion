bf734501f1c2c12b7997ec2b3458be5bcadef057
enigma <205631657+willseeee@users.noreply.github.com>
2026-03-20 16:08:23 +0530

    update

--- check_excel_rows - Copy.js
+++
@@ -1,8 +0,0 @@
-const xlsx = require('xlsx');
-const excelPath = "C:\\Users\\idkth\\Documents\\scion\\assets\\2. Shared with agency folder\\Brands\\MA\\MA Amazon USA Listing.xlsx";
-const workbook = xlsx.readFile(excelPath);
-const sheetName = workbook.SheetNames[0];
-const sheet = workbook.Sheets[sheetName];
-const rawData = xlsx.utils.sheet_to_json(sheet);
-
-console.log(JSON.stringify(rawData.slice(0, 5), null, 2));
