d35e4d99f07c0b9bd82ce61e1e06bee2e99843b6
chanchal7068 <chanchalchauhanjibrand556@gmail.com>
2026-03-24 15:54:04 +0530

    updated

--- script.js
+++ script.js
@@ -8,8 +8,13 @@ $(document).ready(function () {
     cssEase: "linear",
     autoplay: true,
     autoplaySpeed: 5000,
-    arrows: false,
+    arrows: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
     pauseOnHover: false,
+    pauseOnFocus: false,
   });
 
   // Categories Carousel
